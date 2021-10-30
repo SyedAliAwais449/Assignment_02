cc57b727a9398c8837cf0cb72458f3df4011f728
SyedAliAwais449 <saliawaisd449@gmail.com>
2021-10-31 00:13:28 +0500

    CheckBox & Switch Added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //Declaring the Widgets
     EditText editText;
-    TextView valueInPounds;
+    TextView valueInPounds, textView,textView6;
     Button button, button2, button3;
+    CheckBox ck1;
+    Switch switchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,33 @@ public class MainActivity extends AppCompatActivity {
         button= findViewById(R.id.button);
         button2= findViewById(R.id.button2);
         button3= findViewById(R.id.button3);
+        ck1= findViewById(R.id.checkBox2);
+        textView= findViewById(R.id.textView3);
+        textView6= findViewById(R.id.textView6);
+        switchButton= findViewById(R.id.switch2);
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchButton.isChecked()){
+                    textView6.setText(R.string.switchButton);
+                }
+                else{
+                    textView6.setText(R.string.unSwitchButton);
+                }
+            }
+        });
+        ck1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ck1.isChecked()){
+                    textView.setText(R.string.checkButton);
+                }
+                else {
+                    textView.setText(R.string.unCheckButton);
+                }
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
